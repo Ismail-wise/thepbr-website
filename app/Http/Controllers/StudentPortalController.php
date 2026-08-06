e01e009f7511d19621bee85b62ed7db443c5ebe9
Ismail-wise <sithuaung22.pyay@gmail.com>
2026-08-06 17:03:43 +0700

    Add student portal dashboard controller

---
+++ app/Http/Controllers/StudentPortalController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class StudentPortalController extends Controller
+{
+    public function dashboard(Request $request): View
+    {
+        $user = $request->user()->load(['classSession', 'usedAccessCode']);
+
+        return view('student.dashboard', compact('user'));
+    }
+}
