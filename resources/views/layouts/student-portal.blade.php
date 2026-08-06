010d3e6faab2ff1606dadf1db3c56fd61256b067
Ismail-wise <sithuaung22.pyay@gmail.com>
2026-08-06 18:38:43 +0700

    Load partner invitation workspace styles

--- resources/views/layouts/student-portal.blade.php
+++ resources/views/layouts/student-portal.blade.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Myanmar:wght@400;500;600;700&family=Noto+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/student-portal.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/workspace-invitations.css') }}">
 </head>
 <body>
     <header class="portal-header">
