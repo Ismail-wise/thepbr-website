4beb051289bfc06b234f504ab8160fb104512f40
Ismail-wise <sithuaung22.pyay@gmail.com>
2026-08-06 17:48:49 +0700

    Load unified access data in student workspace

--- app/Http/Controllers/StudentPortalController.php
+++ app/Http/Controllers/StudentPortalController.php
@@ -9,7 +9,13 @@ class StudentPortalController extends Controller
 {
     public function dashboard(Request $request): View
     {
-        $user = $request->user()->load(['classSession', 'usedAccessCode']);
+        $user = $request->user()->load([
+            'classSession',
+            'usedAccessCode',
+            'studentEnrollments.classSession',
+            'ownedWorkspaces',
+            'workspaceMemberships.workspace',
+        ]);
 
         return view('student.dashboard', compact('user'));
     }
