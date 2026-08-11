b1cf093c88d6040be88585b84e00cb5977250404
Ismail-wise <sithuaung22.pyay@gmail.com>
2026-08-11 16:22:40 +0700

    Add business valuation model

---
+++ app/Models/BusinessValuation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BusinessValuation extends Model
+{
+    protected $fillable = [
+        'workspace_id',
+        'user_id',
+        'inputs',
+        'result',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'inputs' => 'array',
+            'result' => 'array',
+        ];
+    }
+
+    public function workspace(): BelongsTo
+    {
+        return $this->belongsTo(PartnershipWorkspace::class, 'workspace_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
