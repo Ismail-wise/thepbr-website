f8e2c4b5a3bde00f34f4ce3d03c1a2b8f821d8d9
Ismail-wise <sithuaung22.pyay@gmail.com>
2026-08-12 15:51:20 +0700

    Load startup capital planning design system

--- resources/views/layouts/student-portal.blade.php
+++ resources/views/layouts/student-portal.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="{{ asset('css/pbr-operating-system.css') }}?v={{ filemtime(public_path('css/pbr-operating-system.css')) }}">
     <link rel="stylesheet" href="{{ asset('css/pbr-operating-dashboard.css') }}?v={{ filemtime(public_path('css/pbr-operating-dashboard.css')) }}">
     <link rel="stylesheet" href="{{ asset('css/pbr-operating-fixes.css') }}?v={{ filemtime(public_path('css/pbr-operating-fixes.css')) }}">
+    <link rel="stylesheet" href="{{ asset('css/pbr-startup-capital.css') }}?v={{ filemtime(public_path('css/pbr-startup-capital.css')) }}">
     <link rel="stylesheet" href="{{ asset('css/pbr-roster.css') }}?v={{ filemtime(public_path('css/pbr-roster.css')) }}">
     <script src="{{ asset('js/pbr-operating-system.js') }}?v={{ filemtime(public_path('js/pbr-operating-system.js')) }}" defer></script>
 </head>
